a36ef2e0b3ff0c75fe93017b497023f6a3d5fce3
Tony G <tony1427@msn.com>
2022-10-16 17:49:17 -0500

    Additional files for the initial commit

---
+++ src/test/kotlin/com/walnutvm/motorcycle/MotorcycleApplicationTests.kt
@@ -0,0 +1,15 @@
+package com.walnutvm.motorcycle
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+
+@SpringBootTest
+@ActiveProfiles("test")
+class MotorcycleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
