1a093245f1515b1ac8d0cb1eca71d04194ca5f48
Tony G <tony1427@msn.com>
2022-10-24 22:29:59 -0500

    Fixed issue with the get endpoint. Issue was the no arg constructor was not being seen for dozer mapper. Updated the plugins to include the no arg constructor. Removed DozerMapper due to it will be deprecated in the future. Per their recommendation, changed to model mapper.  Added open-in-view: false to the application yml.

--- src/main/kotlin/com/walnutvm/motorcycle/service/MotorcycleService.kt
+++ src/main/kotlin/com/walnutvm/motorcycle/service/MotorcycleService.kt
@@ -5,17 +5,18 @@ import com.walnutvm.motorcycle.exception.NotFoundException
 import com.walnutvm.motorcycle.model.MotorcycleRepresentation
 import com.walnutvm.motorcycle.repository.MotorcycleRepository
 import com.walnutvm.motorcycle.utils.toUUID
+import org.modelmapper.ModelMapper
 import org.springframework.stereotype.Service
 
 @Service
 class MotorcycleService (
     private val motorcycleRepository: MotorcycleRepository,
-    private val dozerMapper: Mapper
+    private val modelMapper: ModelMapper
         ) {
 
     fun getMotorcycle(id: String): MotorcycleRepresentation {
         return motorcycleRepository.findById(id.toUUID())
-            .map { dozerMapper.map(it, MotorcycleRepresentation::class.java) }
+            .map { modelMapper.map(it, MotorcycleRepresentation::class.java) }
             .orElseThrow{ NotFoundException() }
     }
     fun createMotorcycle(motorcycleRepresentation: MotorcycleRepresentation): MotorcycleRepresentation {
